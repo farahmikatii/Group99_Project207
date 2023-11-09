95261c48de453a55541779e81e36f8bf4999e31f
duahuss <109004925+duahuss@users.noreply.github.com>
2023-11-09 12:07:04 -0500

    Created new api call file, edited main to produce file of all meal types, and created a pom.xml for dependencies

---
+++ src/data_access/APICallDataAccessObject.java
@@ -0,0 +1,21 @@
+package data_access;
+import okhttp3.*;
+import okio.BufferedSink;
+import okio.Okio;
+import java.io.File;
+import java.io.IOException;
+
+public class APICallDataAccessObject {
+
+
+
+
+
+
+}
+
+
+
+
+
+
