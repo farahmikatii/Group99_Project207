f3c7099c13b79909585e55f3cccbb72710d1ffd2
rahman <maleeha.rahman@mail.utoronto.ca>
2023-12-01 22:11:49 -0500

    Lazy loading implemented

--- src/view/LoggedInView.java
+++ src/view/LoggedInView.java
@@ -9,15 +9,26 @@ import interface_adapter.profile.ProfileState;
 import interface_adapter.profile.ProfileViewModel;
 import interface_adapter.recipePopup.RecipePopupState;
 import interface_adapter.recipePopup.RecipePopupViewModel;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okio.BufferedSink;
+import okio.Okio;
+import org.json.JSONObject;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 public class LoggedInView extends JPanel implements ActionListener, PropertyChangeListener {
 
@@ -38,10 +49,14 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
     final JButton search;
     final JButton account;
     JButton recipeImage;
+    String file;
+    private int previousScrollValue = 0;
+    private int prevHorizontalValue = 0;
     public LoggedInView(LoggedInViewModel loggedInViewModel, ViewManagerModel viewManagerModel, ProfileViewModel profileViewModel, RecipePopupViewModel recipePopupViewModel) throws Exception {
         this.loggedInViewModel = loggedInViewModel;
         this.viewManagerModel = viewManagerModel;
         this.profileViewModel = profileViewModel;
+
 
         this.recipePopupViewModel = recipePopupViewModel;
 
@@ -61,12 +76,12 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
         //String jsonFile = "/Users/farahmikati/IdeaProjects/Group99_Project207/response_output.json";
         //String jsonFile = "/Users/duahussain/IdeaProjects/Group99_Project207/response_output.csv";
         //String jsonFile = "/Users/farahmikati/IdeaProjects/Group99_Project207/response_output.json";
-        String jsonFile = "C:/Working/UoFT/Year 2/CSC207/shar2435/Group99_Project207/response_output.json";
-        String file = CommonRecipeDataAccessObject.readFileAsString(jsonFile);
-        CommonRecipeDataAccessObject commonRecipeDAO = new CommonRecipeDataAccessObject(file); // replace jsonFile with the actual JSON file content or path
+        String jsonFile = "C:/Users/rahman/Desktop/Year 2/CSC207 - Software Design/Weekly Activities/Group99_Project207/response_output.json";
+        file = CommonRecipeDataAccessObject.readFileAsString(jsonFile);
+        final CommonRecipeDataAccessObject[] commonRecipeDAO = {new CommonRecipeDataAccessObject(file)}; // replace jsonFile with the actual JSON file content or path
 
         // Call returnRecipeList method
-        List<CommonRecipe> recipesList = commonRecipeDAO.returnRecipeList(1);
+        final List<CommonRecipe>[] recipesList = new List[]{commonRecipeDAO[0].returnRecipeList(1)};
 
 
 
@@ -84,14 +99,14 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
         account = new JButton(loggedInViewModel.ACCOUNT_BUTTON_LABEL);
         //recipesList.get(0).getImage()
         //ImageIcon saveRecipeImage = new ImageIcon(recipesList.get(0).getImage());
-        recipeImage = new JButton(recipesList.get(0).getRecipeName());
+        //recipeImage = new JButton(recipesList.get(0).getRecipeName());
 
 
 
         //Panel topRightButtonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         //searchButton.setLayout(null);
         //accountButton.setLayout(null);
-        recipes.setLayout(new GridLayout(5,4,5,5));
+        recipes.setLayout(new GridLayout(0,4,5,5));
 
 
 
@@ -101,7 +116,7 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
         //divider.add(logOut);
         //recipes.add(recipeImage);
 
-        for (CommonRecipe recipe : recipesList){
+        for (CommonRecipe recipe : recipesList[0]){
             ImageIcon saveRecipeImage = new ImageIcon(recipe.getImage());
             recipeImage = new JButton(recipe.getRecipeName(), saveRecipeImage);
             //setting position of label of recipe
@@ -143,6 +158,124 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
             );
             recipes.add(recipeImage);
         }
+
+        //Lazy loading
+        //scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+
+        scroll.getVerticalScrollBar().addAdjustmentListener(
+                new AdjustmentListener() {
+                    @Override
+                    public void adjustmentValueChanged(AdjustmentEvent e) {
+                        JScrollBar verticalScrollBar = scroll.getVerticalScrollBar();
+                        JScrollBar horizontalScrollBar = scroll.getHorizontalScrollBar();
+
+                        if (verticalScrollBar.getValue() == verticalScrollBar.getMaximum() - verticalScrollBar.getVisibleAmount()) {
+
+                            previousScrollValue = verticalScrollBar.getValue();
+                            prevHorizontalValue = horizontalScrollBar.getValue();
+
+                            JSONObject jsonObject = new JSONObject(file);
+                            JSONObject links = jsonObject.getJSONObject("_links");
+
+
+                            if (!Objects.equals(links.toString(), "{}")) {
+                                JSONObject next = links.getJSONObject("next");
+                                String nextUrl = next.getString("href");
+
+
+                                System.out.println(nextUrl);
+
+                                try{
+                                    OkHttpClient client = new OkHttpClient();
+
+                                    Request request = new Request.Builder()
+                                            .url(nextUrl)
+                                            .get()
+                                            .build();
+
+                                    Response response = client.newCall(request).execute();
+                                    if (response.isSuccessful()) {
+                                        assert response.body() != null;
+                                        //String filePath = "C:/Working/UoFT/Year 2/CSC207/shar2435/Group99_Project207/response_output.json"; // Change the file extension or name as needed
+                                        String filePath = "C:/Users/rahman/Desktop/Year 2/CSC207 - Software Design/Weekly Activities/Group99_Project207/response_output.json";
+
+
+                                        // Write the response to a file
+                                        try (BufferedSink sink = Okio.buffer(Okio.sink(new File(filePath))) ) {
+                                            sink.writeAll(response.body().source());
+                                        } catch (IOException ex) {
+                                            ex.printStackTrace();
+                                        }
+
+                                        System.out.println("API response saved to file: " + filePath);
+                                    }
+                                    response.close();
+                                } catch(IOException ex) {
+                                    try {
+                                        throw new IOException("error");
+                                    } catch (IOException exc) {
+                                        throw new RuntimeException(exc);
+                                    }
+                                }
+
+                                try {
+                                    //String jsonFile = "C:/Users/rahman/Desktop/Year 2/CSC207 - Software Design/Weekly Activities/Group99_Project207/response_output.json";
+                                    file = CommonRecipeDataAccessObject.readFileAsString(jsonFile);
+                                } catch (Exception ex) {
+                                    throw new RuntimeException(ex);
+                                }
+
+                                CommonRecipeDataAccessObject commonRecipeDAO = new CommonRecipeDataAccessObject(file); // replace jsonFile with the actual JSON file content or path
+
+                                // Call returnRecipeList method
+                                recipesList[0] = commonRecipeDAO.returnRecipeList(1);
+
+                                for (CommonRecipe recipe : recipesList[0]){
+                                    ImageIcon saveRecipeImage = new ImageIcon(recipe.getImage());
+                                    recipeImage = new JButton(recipe.getRecipeName(), saveRecipeImage);
+                                    //setting position of label of recipe
+                                    recipeImage.setVerticalTextPosition(SwingConstants.TOP);
+                                    recipeImage.setHorizontalTextPosition(SwingConstants.CENTER);
+                                    recipeImage.addActionListener(
+                                            // This creates an anonymous subclass of ActionListener and instantiates it.
+                                            new ActionListener() {
+                                                public void actionPerformed(ActionEvent evt) {
+
+                                                    if (evt.getSource().equals(recipeImage)) {
+
+                                                        RecipePopupState currentPopupState = recipePopupViewModel.getState();
+                                                        System.out.println(currentPopupState);
+
+                                                        recipePopupViewModel.setState(currentPopupState);
+                                                        System.out.println(recipePopupViewModel.getState());
+
+                                                        recipePopupViewModel.firePropertyChanged();
+
+                                                        viewManagerModel.setActiveView(recipePopupViewModel.getViewName());
+                                                        System.out.println(viewManagerModel.getActiveView());
+                                                        viewManagerModel.firePropertyChanged();
+
+                                                    }
+
+
+                                                }
+                                            }
+                                    );
+                                    recipes.add(recipeImage);
+                                }
+                            }
+
+                            revalidate();
+                            repaint();
+
+                            verticalScrollBar.setValue(previousScrollValue);
+                            horizontalScrollBar.setValue(prevHorizontalValue);
+                            //}
+                        }
+                    }
+                }
+        );
         
         //accountButton.add(account);
         accountButton.setLocation(0,0);
