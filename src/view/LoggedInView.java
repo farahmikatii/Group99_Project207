4a407817b3e5243b51fd615aa5571b28f35c1485
duahuss <duahussainn@gmail.com>
2023-12-02 16:51:28 -0500

    Trying to fix the labels

--- src/view/LoggedInView.java
+++ src/view/LoggedInView.java
@@ -3,7 +3,6 @@ package view;
 import data_access.CommonRecipeDataAccessObject;
 import entity.CommonRecipe;
 import interface_adapter.ViewManagerModel;
-import interface_adapter.logged_in.LoggedInState;
 import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.profile.ProfileState;
 import interface_adapter.profile.ProfileViewModel;
@@ -15,6 +14,7 @@ import okhttp3.Response;
 import okio.BufferedSink;
 import okio.Okio;
 import org.json.JSONObject;
+import use_case.recipePopup.RecipePopupOutputData;
 
 import javax.swing.*;
 import java.awt.*;
@@ -42,6 +42,8 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
     private final RecipePopupViewModel recipePopupViewModel;
 
 
+
+
     JLabel username;
 
 
@@ -59,6 +61,7 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
 
 
         this.recipePopupViewModel = recipePopupViewModel;
+
 
         loggedInViewModel.addPropertyChangeListener(this);
         profileViewModel.addPropertyChangeListener(this);
@@ -138,9 +141,13 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
 //                            }
                             if (evt.getSource() instanceof JButton sourceButton) {
                                 if (evt.getSource().equals(sourceButton)) {
+                                    //recipePopupViewModel.setSelectedRecipeName(selectedRecipeName);
+                                    recipePopupViewModel.setRecipeLabel(recipe.getRecipeName());
 
                                     RecipePopupState currentPopupState = recipePopupViewModel.getState();
+                                    currentPopupState.setRecipeLabel(recipe.getRecipeName());
                                     System.out.println(currentPopupState);
+
 
                                     recipePopupViewModel.setState(currentPopupState);
                                     System.out.println(recipePopupViewModel.getState());
